10ef9f5cd08d83d9034f8e29e5cabd8ed4d2fd38
loftmain <leejinjae7@gmail.com>
2025-02-21 15:18:02 +0900

    shadcn theme provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { ThemeProvider } from "@/providers/theme-provider"
 import localFont from "next/font/local";
 import "./globals.css";
 
@@ -28,7 +29,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            >
         {children}
+        </ThemeProvider>
       </body>
     </html>
   );

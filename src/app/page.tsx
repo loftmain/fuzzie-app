ad72195d059326a4ce487b34d2ca5d39b63aabe6
jinjae <leejinjae7@gmail.com>
2025-02-25 00:55:25 +0900

    landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
+import { ContainerScroll } from "@/components/global/container-scroll-animation";
 import Navbar from "@/components/global/navbar";
 
 export default function Home() {
   return (
-    <main >
+    <main>
       <Navbar />
+      <section
+        className="h-screen w-full bg-neutral-950 rounded-md 
+      !overflow-visible relative flex flex-col items-center antialiased"
+      >
+        <div
+          className="absolute inset-0 h-full w-full items-center px-5 
+        py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_35%,#223_100%)]"
+        >
+          <div className="flex flex-col mt-[-100px] md:mt-[-50px]">
+            <ContainerScroll
+              titleComponent={
+                <div className="flex items-center justify-center flex-col"></div>
+              }
+            ></ContainerScroll>
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
